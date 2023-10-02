dc759edc9c1340fc762f87e8d6fac39f79a0b2d7
Beatriz Gonçalves de Souza <102838758+Beatrizgs1@users.noreply.github.com>
2023-10-02 16:47:37 -0300

    exercicio a

---
+++ exercicios.sql
@@ -0,0 +1,19 @@
+--Exercícios de Fixação - FUNÇÕES
+
+
+--Funções de String:
+--a) Criar e preencher a tabela 'nomes':
+CREATE TABLE nomes (
+    nome VARCHAR(255)
+);
+
+INSERT INTO nomes (nome) VALUES
+    ('Roberta'),
+    ('Roberto'),
+    ('Maria Clara'),
+    ('João');
+
+
+
+
+
